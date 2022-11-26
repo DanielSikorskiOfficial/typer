55d5b2eff22ebf1419f93092e87133e3acfb3282
DanielSikorskiOfficial <daniel.2006@onet.pl>
2022-11-26 21:52:35 +0100

    test

---
+++ danieladas/src/components/Register/Register.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "../Register/Register.module.css";
+import { NavLink } from "react-router-dom";
+function Register() {
+  return (
+    <>
+      <div className={styles.container}>
+        <h2 className={styles.header}>Zarejestruj się</h2>
+        <form>
+          <div className={styles.input}>
+            <input type="text" name="imie" placeholder="Imię" required />
+          </div>
+
+          <div className={styles.input}>
+            <input
+              type="text"
+              name="nazwisko"
+              placeholder="Nazwisko"
+              required
+            />
+          </div>
+          <div className={styles.input}>
+            <input type="text" name="login" placeholder="Login" required />
+          </div>
+          <div className={styles.input}>
+            <input type="email" name="email" placeholder="Email" required />
+          </div>
+          <div className={styles.input}>
+            <input type="password" name="haslo" placeholder="Hasło" required />
+          </div>
+          <div className={styles.form_container}>
+            <button type="submit">Zarejestruj się</button>
+          </div>
+          <div className={styles.footer}>
+            <span className={styles.span}>Masz już konto?</span> <br />
+            <NavLink to={"/"}>
+              <button>Zaloguj się</button>
+            </NavLink>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Register;
